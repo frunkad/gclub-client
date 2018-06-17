e6c6098e4cb62277dc1a61974b6ca904a24a9712
Darshan Baid <darshanbaid22@gmail.com>
2018-06-18 04:57:50 +0530

    firebase updates

--- src/app/layouts/home-layout/home-layout.component.ts
+++ src/app/layouts/home-layout/home-layout.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../../core/auth.service';
 })
 export class HomeLayoutComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit() {
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDd9Qb0Bj_we2sI_Q_ZQY7jbwzOehJjlkA",
+    authDomain: "gu-clubs.firebaseapp.com",
+    databaseURL: "https://gu-clubs.firebaseio.com",
+    projectId: "gu-clubs",
+    storageBucket: "",
+    messagingSenderId: "961772002975"
+ }
 };
